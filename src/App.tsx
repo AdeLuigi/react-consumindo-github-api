d6e433ca9e839cae1b57736dae914503e510f5b7
adeluigi <ademario63@gmail.com>
2020-05-29 19:24:04 -0300

    aplicacao do prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import Routes from './routes/index'
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/index';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Routes/>
+    <Routes />
   </BrowserRouter>
-)
+);
 
 export default App;
