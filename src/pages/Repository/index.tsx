b84013d2fb85510a3be40038e69c1d9e13e54826
adeluigi <ademario63@gmail.com>
2020-05-31 12:41:13 -0300

    estilizaçao da pagina do repositorio

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>descrição do Repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong> <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aspodaspoidopas">
+          <div>
+            <strong>aslkçdasçlkd</strong>
+            <p>dasḱpdasṕkd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
